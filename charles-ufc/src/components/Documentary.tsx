7eed2b356760784fbaff95cde2838a3b06c0a386
DavideCruciani <cruciani240203@gmail.com>
2023-06-16 22:23:44 +0200

    documentary and navbar changed

--- charles-ufc/src/components/Documentary.tsx
+++ charles-ufc/src/components/Documentary.tsx
@@ -2,8 +2,8 @@ const Documentary = () => {
   return (
     <section className="bg-green-700 h-screen" id="documentary">
       <div className="container h-full px-5 py-20 flex flex-col justify-center items-center mx-auto">
-      <iframe className="w-full h-full" src="https://www.youtube.com/embed/DMUsfdibgCc" title="He Was Paralyzed by One Punch : Charles Oliveira Documentary" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-      <small className="text-yellow-400 text-xl font-Lakki mt-4">Video by <a href="https://www.youtube.com/watch?v=DMUsfdibgCc&ab_channel=PatrickGavia">Patrick Gavia</a></small>
+      <iframe className="w-full h-full" src="https://www.youtube.com/embed/pSLBAQVQaYY" title="CHARLES OLIVEIRA - THE MOVIE (Documentary)" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <small className="text-yellow-400 text-xl font-Lakki mt-4">Video by <a href="https://www.youtube.com/watch?v=DMUsfdibgCc&ab_channel=PatrickGavia">MMA Documentaries</a></small>
       </div>
     </section>
   )
--- charles-ufc/src/components/Navbar.tsx
+++ charles-ufc/src/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = ({Logo}: NavbarProps) => {
       <div className='xl:hidden z-[999]' onClick={handleNav}>
       {nav ? <BiX className='cursor-pointer' size={38} color='#FACC15'/> : <BiMenu className='cursor-pointer' size={38} color='#FACC15'/>}
       </div>
-      <div className={nav ? 'fixed bg-green-700 border-l-2 border-l-green-800 flex justify-center py-32 xl:hidden right-0 top-0 text-center p-4 w-2/3 h-full ease-in-out duration-300' : 'fixed border-l-2 border-l-green-800 flex justify-center py-32 xl:hidden top-0 text-center p-4 w-2/3 h-full right-[-100%] ease-in-out duration-300'}>
+      <div className={nav ? 'fixed bg-[#15803D]/[0.9] #172554 border-l-2 border-l-green-800 flex justify-center py-32 xl:hidden right-0 top-0 text-center p-4 w-2/3 h-full ease-in-out duration-300' : 'fixed bg-[#15803D]/[0.9] border-l-2 border-l-green-800 flex justify-center py-32 xl:hidden top-0 text-center p-4 w-2/3 h-full right-[-100%] ease-in-out duration-300'}>
         <ul className="text-yellow-400 flex flex-col gap-10 text-2xl">
           <li className="hover:text-yellow-600"><a href="#">HOME</a></li>
           <li className="hover:text-yellow-600"><a href="#history">HISTORY</a ></li>
