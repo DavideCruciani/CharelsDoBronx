0041dbf1015b6e2005dc4c3730394a1aad71aeea
DavideCruciani <cruciani240203@gmail.com>
2023-06-16 20:03:01 +0200

    Documentary section finished

--- charles-ufc/src/components/Documentary.tsx
+++ charles-ufc/src/components/Documentary.tsx
@@ -1,8 +1,8 @@
 const Documentary = () => {
   return (
     <section className="bg-green-700 h-screen" id="documentary">
-      <div className="container h-full p-5 flex flex-col justify-center items-center mx-auto">
-      <iframe width="1280" height="720" src="https://www.youtube.com/embed/DMUsfdibgCc" title="He Was Paralyzed by One Punch : Charles Oliveira Documentary" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      <div className="container h-full px-5 py-20 flex flex-col justify-center items-center mx-auto">
+      <iframe className="w-full h-full" src="https://www.youtube.com/embed/DMUsfdibgCc" title="He Was Paralyzed by One Punch : Charles Oliveira Documentary" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
       <small className="text-yellow-400 text-xl font-Lakki mt-4">Video by <a href="https://www.youtube.com/watch?v=DMUsfdibgCc&ab_channel=PatrickGavia">Patrick Gavia</a></small>
       </div>
     </section>
--- charles-ufc/src/components/History.tsx
+++ charles-ufc/src/components/History.tsx
@@ -10,26 +10,26 @@ const History = ({Scouter, FirstFight, Champion}: HistoryProps) => {
       <div className="container mx-auto px-5 text-center">
         <div className="flex flex-col-reverse justify-center items-center lg:flex-row  w-full pb-10 pt-20">
           <div className="w-full lg:w-1/2">
-            <img className="border-4 border-yellow-400" src={Scouter} alt="Charles in a scouter" />
+            <img className="w-full h-[500px] object-cover border-4 border-yellow-400" src={Scouter} alt="Charles in a scouter" />
           </div>
           <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
-            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-7xl">From Nothing...</h2>
+            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-6xl">From Nothing...</h2>
           </div>
         </div>
         <div className="flex w-full flex-col lg:flex-row justify-center items-center py-10">
           <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
-            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-7xl">To Something...</h2>
+            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-6xl">To Something...</h2>
           </div>
           <div className="w-full lg:w-1/2">
-            <img className="border-4 border-yellow-400" src={FirstFight} alt="Charles in a scouter" />
+            <img className="w-full h-[500px] object-cover border-4 border-yellow-400" src={FirstFight} alt="Charles in a scouter" />
           </div>
         </div>
         <div className="flex w-full flex-col-reverse lg:flex-row justify-center items-center pt-10 pb-20">
           <div className="w-full lg:w-1/2">
-            <img className="border-4 border-yellow-400 object-cover" src={Champion} alt="Charles in a scouter" />
+            <img className="w-full h-[500px] object-cove border-4 border-yellow-400 object-cover" src={Champion} alt="Charles in a scouter" />
           </div>
           <div className="w-full lg:w-1/2 flex items-center justify-center p-4">
-            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-7xl">To Everything</h2>
+            <h2 className="text-yellow-400 font-Lakki xl:text-9xl sm:text-8xl text-6xl">To Everything</h2>
           </div>
         </div>
       </div>
